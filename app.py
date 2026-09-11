1b9f8dbc26921db696fe12ce67d880459a9c8053
Noor Sajid <noorsajidns324@gmail.com>
2026-09-12 00:26:11 +0500

    Update Aiven database configuration

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ CORS(app)
 # -------------------------
 # MySQL Configuration
 # -------------------------
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "root"
-app.config["MYSQL_DB"] = "lms"
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB", "defaultdb")
+
+app.config["MYSQL_CUSTOM_OPTIONS"] = {
+    "ssl_mode": "REQUIRED"
+}
 
 mysql = MySQL(app)
 
